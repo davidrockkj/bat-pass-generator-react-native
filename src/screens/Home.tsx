281b4e6ea918fac010a96cef46c38a2b475c1e78
davidrockkj <davidrockkj@gmail.com>
2024-01-25 18:04:21 -0300

    Criando primeira 'screen' Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Home() {
   return(
-    <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
