1cb82720416c26fa174c897549b755378a39f60e
davidrockkj <davidrockkj@gmail.com>
2024-01-25 18:31:38 -0300

    Removendo o componente 'Menu'

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet } from 'react-native';
 import Home from './src/screens/Home/Home';
-import { Menu } from './src/components/Menu/Menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Menu />
       <Home />
       <StatusBar style="auto" />
     </View>
