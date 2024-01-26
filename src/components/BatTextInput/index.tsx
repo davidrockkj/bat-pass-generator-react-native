0c3d0d34aa28fae2e2c903d2efb04c41f28836fa
unknown <davidrockkj@gmil.com>
2024-01-26 11:48:48 -0300

    Criando um TextInput dentro de 'BatTextInput'

--- src/components/BatTextInput/index.tsx
+++ src/components/BatTextInput/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 
 import { styles } from './styles';
 
 export function BatTextInput() {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <TextInput placeholder='password' />
   );
 }
