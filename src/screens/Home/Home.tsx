47366c5ea29bb2625158944d4aba5b37124354cc
unknown <davidrockkj@gmil.com>
2024-01-26 12:42:16 -0300

    Implementando o BatButton

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 import styles from "./style";
 import { BatLogo } from "../../components/BatLogo/BatLogo";
 import { BatTextInput } from "../../components/BatTextInput";
+import { BatButton } from "../../components/BatButton/BatButton";
 
 export default function Home() {
   return(
@@ -13,6 +14,7 @@ export default function Home() {
 
       <View style={styles.inputcontainer}>
         <BatTextInput />
+        <BatButton />
       </View>
 
       <Text style={{color: 'black'}}>Home</Text>
