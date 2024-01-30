7e8fb7bdf2cd2be17f559bfeb237b498ccf0b760
unknown <davidrockkj@gmil.com>
2024-01-29 21:56:51 -0300

    InputText - editable={false} -> removendo a possibilidade de digitar dentro do input

--- src/components/BatTextInput/index.tsx
+++ src/components/BatTextInput/index.tsx
@@ -14,6 +14,7 @@ export function BatTextInput(props: BatTextInputProps) {
       style={styles.input}
       placeholder='password'
       value={props.password}
+      editable={false}
       />
   );
 }
